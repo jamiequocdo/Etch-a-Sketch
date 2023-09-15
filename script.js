03b579540ab294e3fa3183f7022448b5c6815e73
Jamie <jamiedofitness@gmail.com>
2023-09-14 20:23:04 -0700

    Browser now takes prompt and makes Squares

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-const maxSquares = 16;
+//prompt from user for how big of grid a person wants.  start with 1
+
+
+
+const maxSquares = parseInt(prompt("How big of a square"));
+console.log(typeof maxSquares);
 
 for (let i = 0; i < maxSquares; i++) {
-    const squared = document.getElementById("boxOfDivs");
+    const squared = document.getElementById("div-container");
+    const computedStyle = window.getComputedStyle(squared);
+    const heightSquared = parseInt(computedStyle.getPropertyValue("height"));
+    const widthSquared = parseInt(computedStyle.getPropertyValue("width"));
     let square = document.createElement("div");
     square.classList.add("squareStyle");
+    square.style.height = (heightSquared / maxSquares) + "px"
+    square.style.width = (widthSquared / maxSquares) + "px";
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "white";
+    
+    })
     squared.appendChild(square);
 } 
-
-const squared = document.getElementById("boxOfDivs");
-
-squared.addEventListener("mouseover", () => {
-    squared.classList.add("hoverDiv");
-    console.log("hovering")
-
-})
