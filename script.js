3084b37a33d1d2f85b05d7b4aec2bdf0c5276df6
jamiequocdo <79014033+jamiequocdo@users.noreply.github.com>
2023-09-18 18:25:12 -0700

    Create Rainbow button.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let maxSquares = ""
+let maxSquares = "";
 
 const gridSize = document.getElementById("gridSize");
 gridSize.addEventListener("click", createGrid);
 
+//Creates new grid of squares.  If one present, deletes old, makes new.
 function createGrid() {
 
-    //removes old grid
-    const gridSquares = document.querySelectorAll(".squareStyle"); //add this
-    gridSquares.forEach(child => { //add this
-        child.remove(); //add this
+    const gridSquares = document.querySelectorAll(".squareStyle");
+    gridSquares.forEach(child => { 
+        child.remove(); 
     })
 
     maxSquares = parseInt(prompt("How big is the grid?"));
@@ -22,37 +22,59 @@ function createGrid() {
         square.classList.add("squareStyle");
         square.style.height = (heightSquared / maxSquares) + "px"
         square.style.width = (widthSquared / maxSquares) + "px";
-        /*square.addEventListener("mouseover", () => {
+        //This changes the square from black to white. this should be default
+        square.addEventListener("mouseover", () => {
             square.style.backgroundColor = "white";
-        })*/
-        
+        })
+    squared.appendChild(square);
+
+    }
+}
+//Mouseover square elements now changes color to a random color
+const rainbowButton = document.getElementById("rainbowButton");
+rainbowButton.addEventListener("click", toggleRainbowSquares);
+function toggleRainbowSquares() {
+    const allSquares = document.querySelectorAll(".squareStyle");
+    allSquares.forEach(element => {
+        let redValue = ""
+        let greenValue = "";
+        let blueValue = "";
+        let rgbNames = [redValue, greenValue, blueValue];
+        let rgbValues = [];
+        rgbNames.forEach(value => {
+            value = Math.floor(Math.random() * 255);
+            rgbValues.push(value);
+        })
+        element.addEventListener("mouseover", () => {
+            element.style.backgroundColor = `rgb(${rgbValues[0]}, ${rgbValues[1]}, ${rgbValues[2]})`
+        })
+    })
+}
+
+
+        /*
+        This changes square to a random color continuously.  still need to have it so when rainbow button pressed, it occurs
+        square.addEventListener("mouseover", () => {
+            */
+
+    /* for 10% increase in shading, still need to create a maximum to 100%. also need to have this only trigger when shader button pressed
+        let redValue = 0;
+        let greenValue = 0;
+        let blueValue = 0;
 
         square.addEventListener("mouseover", () => {
-            let redValue = ""
-            let greenValue = "";
-            let blueValue = "";
+            
+            redValue += 10;
+            greenValue += 10;
+            blueValue += 10;
+            console.log(redValue);
             let rgbNames = [redValue, greenValue, blueValue];
-            let rgbValues = [];
-            rgbNames.forEach(value => {
-                value = Math.floor(Math.random() * 255);
+            square.style.backgroundColor = `rgb(${rgbNames[0]}%, ${rgbNames[1]}%, ${rgbNames[2]}%)`; */
+
+            /*rgbNames.forEach(value => {
+                if (value <= 255) {
+                value += 10;
                 rgbValues.push(value);
-            })
-            square.style.backgroundColor = `rgb(${rgbValues[0]}, ${rgbValues[1]}, ${rgbValues[2]})`
-        })
-
-    squared.appendChild(square);
-    }
-
-} 
-
-
-
-/* Rainbow Mouseover
-- Mouseover a square, a random color shows is shown
-    - Create a button that will trigger the mouse to turn squares into random colors
-    - Create EventListener for MouseOver
-        - Object needs to be selected with document.getElement
-    - Event Listener will pick color using RGB color
-        - RGB color needs 3 random numbers
-            - produce 3 random numbers (Done)
-*/
+                }
+                console.log(rgbValues);
+            })*/
